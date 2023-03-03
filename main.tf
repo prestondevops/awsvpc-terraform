3a25315dc4517433bcdfa266082d91451b2a43e0
Stan Moriasi <stanmoriasi@yahoo.com>
2023-03-02 17:33:06 -0700

    updated aws_instance name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web3" {
   }
   
 }
-resource "aws_instance" "web3" {
+resource "aws_instance" "web4" {
   ami = "ami-006dcf34c09e50022" 
   instance_type = "t2.micro"
   tags = {
